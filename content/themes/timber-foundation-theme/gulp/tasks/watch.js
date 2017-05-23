67df94fa0843de0cd157f29abc0e12775717e1b5
Zachary Hickson <zachary@Zacharys-MacBook-Pro.local>
2017-05-24 09:24:44 +1000

    removed timber library and starter theme

--- content/themes/timber-foundation-theme/gulp/tasks/watch.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('watch', ['setWatch'], function() {
-	gulp.watch('src/scss/**', ['styles']);
-	gulp.watch('src/img/**', ['images']);
-	gulp.watch('src/js/**', ['scripts']);
-	// Note: The browserify task handles js recompiling with watchify
-});
--- content/themes/timber-foundation-theme/sidebar.php
+++
@@ -1,4 +0,0 @@
-<?php
-//$context = array();
-//$context['sidebar'] = Timber::get_widgets('sidebar');
-//Timber::render('sidebar.twig', $context);
